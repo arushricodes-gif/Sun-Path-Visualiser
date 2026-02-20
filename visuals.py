3b987ba2f6ff5cf2d26e4939ff725852477f881e
Arushri Gangji <arushriprojects@gmail.com>
2026-02-20 13:12:48 +0400

    Update visuals.py

--- visuals.py
+++ visuals.py
@@ -120,6 +120,7 @@ def render_map_component(lat, lon, radius_meters, path_data, animate_trigger, si
             info.onAdd = function() {{
                 var div = L.DomUtil.create('div', 'map-stats-card');
                 div.innerHTML = `<div>🌅Sunrise: <b>{rise_time}</b></div><div>🌇Sunset: <b>{set_time}</b></div><div style="color:#F39C12;">💨 AQI: <b>{aqi_val}</b></div>`;
+                div.innerHTML = `<div>🔴 Sunrise Line</div><div>🔵 Sunset Line</div><div>⚪ Shadow Line></div>`;
                 return div;
             }};
             info.addTo(map2);
